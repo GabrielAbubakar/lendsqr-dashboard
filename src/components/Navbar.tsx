3c3dbadd9b74d6b650d6fa294c7617ca793f9688
Gabriel Abubakar <gabrielogrima@gmail.com>
2023-01-01 11:10:30 +0100

    navbar component implemented

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import Logo from '../assets/lendsqr-nav-logo.svg'
+import Profile from '../assets/profile-image.png'
+import SearchIcon from '../assets/icons/search-icon.svg'
+import NotificationIcon from '../assets/icons/notification-icon.svg'
+import DropdownIcon from '../assets/icons/dropdown-icon.svg'
+
+const Navbar = () => {
+    return (
+        <nav className='navigation'>
+
+            <div className='navigation__logo-box'>
+                <img src={Logo} alt="lendsqr logo" title='lendsqr logo' />
+            </div>
+
+            <div className='navigation__input-group'>
+                <input
+                    type="text"
+                    placeholder="Search for anything" />
+                <button>
+                    <img src={SearchIcon} alt="search" title='search' />
+                </button>
+            </div>
+
+            <ul className='navigation__links'>
+                <li>Docs</li>
+                <li>
+                    <img src={NotificationIcon} alt="notification bell" />
+                </li>
+                <li className='navigation__links-profileBox'>
+                    <div>
+                        <img src={Profile} alt="profile image" />
+                    </div>
+                    <p>
+                        Adedeji
+                        <img src={DropdownIcon} alt="dropdown icon" />
+                    </p>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import Navbar from "../components/Navbar"
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Dashboard
